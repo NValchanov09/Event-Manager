cb6376cf4f5e07bf6a748d7f05e4db9a7849abac
Nikolay Valchanov <nikolay.valchanov09@gmail.com>
2025-07-23 17:24:47 +0300

    Changed the redirect of not-found to the home page, not events page

--- Frontend/src/views/NotFoundView.vue
+++ Frontend/src/views/NotFoundView.vue
@@ -2,7 +2,7 @@
 	<div
 		class="w-full h-full overflow-auto bg-grey text-white flex flex-col justify-center items-center px-6 py-12 text-center gap-y-8">
 		<h1
-			class="text-[96px] sm:text-[120px] leading-none font-black text-yellow">
+			class="text-4xl sm:text-[120px] leading-none font-black text-yellow">
 			404
 		</h1>
 		<h2 class="text-3xl md:text-4xl font-semibold">
@@ -12,9 +12,9 @@
 			Страницата, която търсите, не съществува или е била преместена.
 		</p>
 		<RouterLink
-			to="/events"
+			to="/"
 			class="inline-block bg-yellow text-grey px-6 py-3 rounded-lg font-semibold hover:opacity-90 transition whitespace-nowrap">
-			Назад към събития
+			Към началната страница
 		</RouterLink>
 	</div>
 </template>
